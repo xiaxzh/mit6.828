2dd4a0718d5152dcd584ad330178977618d29d24
xiaxzh <451291165@qq.com>
2020-10-26 13:23:10 +0800

    fix lab3 remain pg_fault_handler

--- lab3/kern/trap.c
+++ lab3/kern/trap.c
@@ -265,6 +265,9 @@ page_fault_handler(struct Trapframe *tf)
 	// Handle kernel-mode page faults.
 
 	// LAB 3: Your code here.
+	if ((tf->tf_cs & 0x01) == 0) {
+		panic("pg fault in kernel mode, fault in va %08x\n", fault_va);
+	}
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
